a0c99610671c85711689692740a20d88e02177a0
Svitlovskyi <maksym.svitlovskyi@gmail.com>
2020-11-29 01:10:39 +0100

    Add camera view

--- scannerApp/ViewController.swift
+++ scannerApp/ViewController.swift
@@ -9,36 +9,10 @@ import UIKit
 import AVFoundation
 import Firebase
 
-class ScaledElementProcessor {
-    
-    
-    let vision = Vision.vision()
-    var textRecognizer: VisionTextRecognizer!
-    
-    init() {
-        self.textRecognizer = vision.onDeviceTextRecognizer()
-
-    }
-    
-    func process(in imageView: UIImageView,
-                 callback: @escaping (_ text: String) -> Void) {
-
-        guard let image = imageView.image else { return }
-        
-        
-        let visionImage = VisionImage(image: image)
-        
-        textRecognizer.process(visionImage) { result, error in
-            guard error == nil,
-                let result = result,
-                !result.text.isEmpty
-            else {
-                callback("")
-                return
-            }
-            callback(result.text)
-        }
-    }
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
 }
 
 class ViewController: UIViewController {
@@ -46,19 +20,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let cameraView = CameraView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.screenWidth, height: UIScreen.screenHeight))
+        cameraView.center = self.view.center
+
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized: // The user has previously granted access to the camera.
+                
+                self.view.addSubview(cameraView)
+
+            case .notDetermined: // The user has not yet been asked for camera access.
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        self.view.addSubview(cameraView)
+                    }
+                }
+            
+            case .denied: // The user has previously denied access.
+                return
+
+            case .restricted: // The user can't grant access due to restrictions.
+                return
+        }
+        
+        
         // Do any additional setup after loading the view.
         
-        let processor = ScaledElementProcessor()
-        processor.process(in: UIImageView(image: UIImage(named: "test_image_1")), callback: { text in
-            print(text)
-        })
-        
-        processor.process(in: UIImageView(image: UIImage(named: "test_image_2")), callback: { text in
-            print(text)
-        })
-        
+//        let processor = ScaledElementProcessor()
+//        processor.process(in: UIImageView(image: UIImage(named: "test_image_1")), callback: { text in
+//            print(text)
+//        })
+//
+//        processor.process(in: UIImageView(image: UIImage(named: "test_image_2")), callback: { text in
+//            print(text)
+//        })
+//
     }
-    
-    
 }
 
